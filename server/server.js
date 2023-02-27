fa0c69d9d9d98d7843a14b16ebf2c6a5857240fa
didi6135 <dydyd289@gmail.com>
2023-02-27 14:50:50 +0200

    first-commit

---
+++ server/server.js
@@ -0,0 +1,65 @@
+ 
+import  express  from "express";
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from "openai";
+
+
+dotenv.config()
+
+const configoretion = new Configuration({
+    apiKey: process.env.OPEN_API_KEY,
+})
+
+const openai = new OpenAIApi(configoretion)
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', async(req,res) => {
+    res.status(200).send({
+        message: 'Hello from codex'
+    })
+})
+
+app.post('/', async(req, res) => {
+    try {
+        const prompt = req.body.prompt
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+        })
+
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        })
+    } catch (err) {
+        console.log(err)
+        req.status(500).send({err})
+    }
+})
+
+app.listen(5000, () => console.log('serveer is running on port http://localhost:5000'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
