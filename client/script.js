d5a87bf0f7cad59976534a977362d9a02c62b62b
didi6135 <41243563+didi6135@users.noreply.github.com>
2023-02-28 17:26:57 +0200

    Update script.js

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv)
 
-  const response = await fetch('https://chat-gpt-vhhe.onrender.com' , {
+  const response = await fetch('https://chat-gpt-vtha.onrender.com' , {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
